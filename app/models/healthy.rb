a2df87ecc7d8689fce1256db318d8c5d48e501bc
shtzr840329 <shtzr840329@gmail.com>
2015-08-09 16:52:12 +0800

    fix healthy...

--- app/models/healthy.rb
+++ app/models/healthy.rb
@@ -3,9 +3,22 @@ class Healthy < BaseForecast
   def get_message
     content = get_data
     shows = ["COPD患者", "儿童哮喘", "老年人感冒"]
-    content.each do |item|
-      
+    now_day = Time.zone.now
+    results = now_day.strftime("%Y-%m-%d")
+    content[0].each do |item|
+      if shows.include?(item["title"])
+        results << "#{item['title']}气象风险等级:"
+        results << "#{item['level']}级 #{item['guide']}\n"
+      end
     end
+    results << "\r\n"
+    results << (now_day + 1.day).strftime("%Y-%m-%d")
+    content[0].each do |item|
+      results << "#{item['title']}气象风险等级:"
+      results << "#{item['level']}级 #{item['guide']}\n"
+    end
+
+    { :type => 'text', :content => results }
   end
 
   def initialize
@@ -20,5 +33,5 @@ class Healthy < BaseForecast
   end
 
   private
-  
+
 end
