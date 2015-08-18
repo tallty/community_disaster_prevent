43bb9d162dbf05a77800c6f0b7cced05cc97c23b
shtzr840329 <shtzr840329@gmail.com>
2015-08-18 20:47:01 +0800

    fix atten view

--- app/models/base_weixin.rb
+++ app/models/base_weixin.rb
@@ -1,6 +1,6 @@
 module BaseWeixin
   def binding_community
-    { :type => 'articles', :content => [{ :title => "社区绑定", :desc => "", :image_url => "#{Settings.ProjectSetting.url}/assets/community2.png", :page_url => "#{Settings.ProjectSetting.url}/subscribers/new?openid=#{@subscriber}" }]}
+    { :type => 'articles', :content => [{ :title => "社区绑定", :desc => "", :image_url => "#{Settings.ProjectSetting.url}/assets/images/community2.png", :page_url => "#{Settings.ProjectSetting.url}/subscribers/new?openid=#{@subscriber}" }]}
   end
 
   def weixin_url(url)
