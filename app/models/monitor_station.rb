a48c5e3f43ed896adfcfb8834ee8e3a1ce4687ee
shtzr840329 <shtzr840329@gmail.com>
2015-09-10 21:57:20 +0800

    fix

--- app/models/monitor_station.rb
+++ app/models/monitor_station.rb
@@ -38,11 +38,7 @@ class MonitorStation < ActiveRecord::Base
       auto_station = MonitorStation.where(community: subscriber.community, station_type: "自动站").first
       url = "#{base_url}&type=s_auto_station&sitenumber=#{auto_station.station_number}"
       data = get_data url
-      p "-----------------------------自动站点--------------------------------------"
       $redis.hset("monitor_stations", auto_station.station_number, data) if data.present?
-      p "data: #{data}"
-      p "-------------------------------------------------------------------------------"
-      # data = $redis.hget("monitor_stations", auto_station.station_number)
       if data.present?
         data = MultiJson.load data
         results << { :title => "温度: #{data["tempe"]} ℃", :desc => "", :image_url => "#{Settings.ProjectSetting.url}/assets/images/temp.png", :page_url => weixin_url("monitor_stations") }
@@ -53,14 +49,8 @@ class MonitorStation < ActiveRecord::Base
       water_stations.each do |item|
         url = "#{base_url}&type=s_water_stations&sitenumber=#{item.station_number}"
         data = get_data url
-
-        p "-----------------------------积水站点--------------------------------------"
         $redis.hset("monitor_stations", item.station_number, data) if data.present?
-        p "data: #{data}"
-        p "-------------------------------------------------------------------------------"
-        
-        # data = $redis.hget("monitor_stations", item.station_number)
-        # data = MultiJson.load data 
+      
         max_deep = data["data"] if data["data"].to_f > max_deep
       end
       results << { :title => "积水: #{max_deep} m", :desc => "", :image_url => "#{Settings.ProjectSetting.url}/assets/images/water.png", :page_url => weixin_url("monitor_stations") }
@@ -104,6 +94,58 @@ class MonitorStation < ActiveRecord::Base
     end
   end
 
+  def self.translate_wspeed speed
+    speed = speed.to_f
+    if (0.0..0.2).include? speed
+      "静风"
+    elsif (0.2..1.5).include? speed
+      "1级"
+    elsif (1.5..3.3).include? speed
+      "2级"
+    elsif (3.3..5.4).include? speed
+      "3级"
+    elsif (5.4..7.9).include? speed
+      "4级"
+    elsif (7.9..10.7).include? speed
+      "5级"
+    elsif (10.7..13.8).include? speed
+      "6级"
+    elsif (13.8..17.1).include? speed
+      "7级"
+    elsif (17.1..20.7).include? speed
+      "8级"
+    elsif (20.7..24.4).include? speed
+      "9级"
+    elsif (24.4..28.4).include? speed
+      "10级"
+    elsif (28.4..32.6).include? speed
+      "11级"
+    elsif (32.6..100).include? speed
+      "12级"
+    end
+  end
+
+  def self.translate_wdir direction
+    direction = direction.to_f
+    if (0..22.5).include?(direction) || (337.5..360).include?(direction)
+      "北风"
+    elsif (22.5..67.5).include? direction
+      "东北风"
+    elsif (67.5..112.5).include? direction
+      "东风"
+    elsif (112.5..157.5).include? direction
+      "东南风"
+    elsif (157.5..202.5).include? direction
+      "南风"
+    elsif (202.5..247.5).include? direction
+      "西南风"
+    elsif (247.5..292.5).include? direction
+      "西风"
+    elsif (292.5..337.5).include? direction
+      "西北风"
+    end
+  end 
+  
   private
   def base_url
     "http://61.152.122.112:8080/publicdata/data?appid=LV08MwglXetHcxdaUTIR&appkey=3LpFnUP84xhj5HaIcmKGAC2yezMgY9"
