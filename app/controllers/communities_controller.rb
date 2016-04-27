89242cbf9b796a3482296c51cdcbe337bb2c2f12
zhangliugang <www.zhangyi508@gmail.com>
2016-04-27 14:37:32 +0800

    fix bug map

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -4,6 +4,7 @@ class CommunitiesController < ApplicationController
 
 	# 社区四个部分
 	def index
-		
+		@subscriber = Subscriber.where(openid: params[:openid]).first
+    @disasters = Disaster.where("occur_time > ?", Time.now.to_date - 3.day).find_all { |d| d.subscriber == @subscriber or d.disaster_position.present? }
 	end
 end
