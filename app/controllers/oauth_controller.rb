deba5cce75906712bec1f28c6ba415570559416e
shtzr840329 <shtzr840329@gmail.com>
2016-07-11 13:44:52 +0800

    debug oauth

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -14,6 +14,7 @@ class OauthsController < ApplicationController
   def check
     result = $client.get_oauth_access_token(params[:code])
     openid = result.result['openid']
+    p "=====================openid: #{openid}============================"
     session[:openid] = openid
     redirect_to session[:reurl]
   end
